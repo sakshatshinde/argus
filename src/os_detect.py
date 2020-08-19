c419643750670058880824eabb2be883023ed1bd
sakshatshinde <shindesakshat@gmail.com>
2020-08-19 19:23:01 +0530

    Adding root detection
    It's not working on my end, maybe due to my system is an experimental distribution. Need to check it later on a stable box.

--- src/os_detect.py
+++ src/os_detect.py
@@ -6,9 +6,23 @@ class operating_system:
     # class is actually used to generate an object
 
         import distro
+        import os
 
-        os = distro.linux_distribution()
-        self.name, self.version, self.release = os 
+        # Identifying the linux distribution and its details
+        os_identifiers = distro.linux_distribution()
+        self.name, self.version, self.release = os_identifiers
+
+        # Checking the program perms. SEEMS TO BE BROKEN ON MY END
+        def _perm_check():
+            os_uid = os.getuid()
+            if os_uid == 0 or os_uid == None:
+                raise NameError('Please run this with root user permisions')
+                    
+            else:
+                print('Running with root user perms, as expected ...')
+        
+        _perm_check()
+        # print(os)
 
     def __repr__(self):
         return repr(
@@ -16,5 +30,12 @@ class operating_system:
         )
     
     def _details(self):
-        print(self.name, self.version, self.release)
+        try:
+            print(self.name, self.version, self.release)
+        except:
+            print('Error detecting the platform')
 
+
+
+os = operating_system()
+print(os._details())
