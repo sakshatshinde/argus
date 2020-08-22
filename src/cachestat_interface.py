9d8ff88295063378dcfc367a8400acab2b253dc3
sakshatshinde <shindesakshat@gmail.com>
2020-08-22 23:54:32 +0530

    Added regex
    Parsing values out of cachestat tool into python

--- src/cachestat_interface.py
+++ src/cachestat_interface.py
@@ -4,7 +4,8 @@ class cachestat:
     # imported inside the class because we only need it when 
     # class is actually used to generate an object
     import subprocess
-            
+    import re 
+
     # Default time interval is set to 5
     cmd = ['../monitoring_scripts/cachestat.sh', '5']
 
@@ -20,10 +21,29 @@ class cachestat:
         return iter(cachestat_process.stdout.readline, b'')
 
 
+    def __regex_check(self,input_string: str):
+
+        # Check if we got a correct input first, only then we can process it in regex
+        regex_check = "(hits:)"
+        result_check = self.re.search(regex_check, input_string)
+
+        # Now matching the actual regex and extracting values
+        if result_check:
+            regex = "(\d+)"
+            result = self.re.findall(regex, input_string)
+            if result: print(result)
+        else:
+            pass
+
     # Capturing cachestat input upto a certain line count then killing the process
-    def acquire_cachestat_data(self, count = 0, count_upto = 10):
+    def acquire_cachestat_data(self, count = 0, count_upto = 4):
         for line in self.__run_cachestat(self.cmd):
-            print(line)
+            print(line,'\n')
+
+            input_string = str(line)
+            self.__regex_check(input_string)
+
+            # Limiting the amount of time the metrics are captured
             count = count + 1
             if count > count_upto: 
                 cachestat_process.kill() 
@@ -31,5 +51,6 @@ class cachestat:
         
     
 
-# cs = cachestat()
-# cs.acquire_cachestat_data()
+cs = cachestat()
+cs.acquire_cachestat_data()
+# (hits:).+(\d+), +([a-z]+): +(\d+), +([a-z]+): +((\d)+\.(\d)%), +[a-z]+_[a-z]+:.+\d
