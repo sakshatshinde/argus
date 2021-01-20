5ed358882883c60909f671218b28ac7e94a986c8
Sakshat <30901276+sakshatshinde@users.noreply.github.com>
2021-01-20 16:53:26 +0530

    Update retrieve_data.py

--- src/retrieve_data.py
+++ src/retrieve_data.py
@@ -1,17 +1,19 @@
 from firebase import firebase  
 import pprint
 
+
 # setting up prettyPrint: because the o/p is messy
 pp = pprint.PrettyPrinter(indent=4)
 
 # creating the connection
 fb = firebase.FirebaseApplication('https://cdac-argus-default-rtdb.firebaseio.com/', None)  
-
-# metrics = db.reference('metrics')
 result = fb.get('cdac-argus-default-rtdb/metrics', None)
 
 # seperating data
-cachestat_data = []
+cachestat_data_ms = []
+cachestat_data_IO = [] 
+
+# this is seperated from source so no need to seperate
 io_latency_data = []
 
 for val in result.values():
@@ -22,15 +24,38 @@ for val in result.values():
 
     # extracting metrics
     if (metrics[0].startswith("ms_range")):
-        cachestat_data.append(metrics[0])
+        # cachestat_data.append(metrics[0])
+        ms_range_metrics, IO_metrics = metrics[0].split("IO", 1)
+
+        # A very hacky fix to remove ms_range attached to the list so the EVAL() works in the code beneath 
+        garbage, ms_range_metrics = ms_range_metrics.split("ms_range")
+        
+        # storing cleaned and sorted data
+        cachestat_data_ms.append(eval(ms_range_metrics))
+        cachestat_data_IO.append(eval(IO_metrics))
+
     elif (metrics[0].startswith("{")):
-        io_latency_data.append(metrics[0])
+        
+        # storing cleaned and sorted data
+        io_latency_data_cleaned = eval(metrics[0])
+        io_latency_data.append(io_latency_data_cleaned)
 
 
-pp.pprint(cachestat_data)
+
+print("-------------------------------------------------")
+print("Cachestat MS RANGE DATA")
+print("-------------------------------------------------")
+pp.pprint( cachestat_data_ms)
+
+print("-------------------------------------------------")
+print("Cachestat IO DATA")
+print("-------------------------------------------------")
+pp.pprint(cachestat_data_IO)
+
+print("-------------------------------------------------")
+print("IO_LATENCY IO DATA")
 print("-------------------------------------------------")
 pp.pprint(io_latency_data)
-
 
 # pp.pprint(result)
 # metric_data = result.values()
