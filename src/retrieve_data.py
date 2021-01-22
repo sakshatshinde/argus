0780b94054671c0b49a197245c8f130089302333
Sakshat <30901276+sakshatshinde@users.noreply.github.com>
2021-01-22 16:36:54 +0530

    Update retrieve_data.py

--- src/retrieve_data.py
+++ src/retrieve_data.py
@@ -1,5 +1,6 @@
 from firebase import firebase  
 import pprint
+import mysql.connector
 
 #---------------READ THIS-----------------------------------
 # There is a mistake in this code, the tools are misllabled
@@ -21,46 +22,93 @@ cachestat_data_IO = []
 # this is seperated from source so no need to seperate
 io_latency_data = []
 
-for val in result.values():
+def get_data():
+    for val in result.values():
 
-    # easiest way to get the values since firebase adds its own key
-    # nesting(complicating) the already awful dict structure
-    metrics = (list(val.values()))
+        # easiest way to get the values since firebase adds its own key
+        # nesting(complicating) the already awful dict structure
+        metrics = (list(val.values()))
 
-    # extracting metrics
-    if (metrics[0].startswith("ms_range")):
-        # cachestat_data.append(metrics[0])
-        ms_range_metrics, IO_metrics = metrics[0].split("IO", 1)
+        # extracting metrics
+        if (metrics[0].startswith("ms_range")):
+            # cachestat_data.append(metrics[0])
+            ms_range_metrics, IO_metrics = metrics[0].split("IO", 1)
 
-        # A very hacky fix to remove ms_range attached to the list so the EVAL() works in the code beneath 
-        garbage, ms_range_metrics = ms_range_metrics.split("ms_range")
-        
-        # storing cleaned and sorted data
-        cachestat_data_ms.append(eval(ms_range_metrics))
-        cachestat_data_IO.append(eval(IO_metrics))
+            # A very hacky fix to remove ms_range attached to the list so the EVAL() works in the code beneath 
+            garbage, ms_range_metrics = ms_range_metrics.split("ms_range")
+            
+            # storing cleaned and sorted data
+            cachestat_data_ms.append(eval(ms_range_metrics))
+            cachestat_data_IO.append(eval(IO_metrics))
 
-    elif (metrics[0].startswith("{")):
+        elif (metrics[0].startswith("{")):
 
-        # storing cleaned and sorted data
-        io_latency_data_cleaned = eval(metrics[0])
-        io_latency_data.append(io_latency_data_cleaned)
+            # storing cleaned and sorted data
+            io_latency_data_cleaned = eval(metrics[0])
+            io_latency_data.append(io_latency_data_cleaned)
+
+    
+
+    # print("-------------------------------------------------")
+    # print("IO_LATENCY MS_RANGE DATA")
+    # print("-------------------------------------------------")
+    # pp.pprint(cachestat_data_ms)
+
+    # print("-------------------------------------------------")
+    # print("IO_LATENCY IO DATA")
+    # print("-------------------------------------------------")
+    # pp.pprint(cachestat_data_IO)
+
+    # print("-------------------------------------------------")
+    # print("CACHESTAT DATA")
+    # print("-------------------------------------------------")
+    # pp.pprint(io_latency_data)
+
+get_data()
+
+local_mysql_db = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "12345",
+        database = "cdac_argus"
+    )
+
+mycursor = local_mysql_db.cursor()
+
+def insert_data_cachestat_mysql(values):
+    query = "INSERT INTO cachestat (hits, misses, mbd, ratio, buffers_mb, cached_mb) VALUES (%s,%s,%s,%s,%s,%s)"
+    # values = (65,1347,21502,27,0,100)
+
+    mycursor.execute(query,values)
+    local_mysql_db.commit()
 
 
+# insert_data_cachestat_mysql()
 
-print("-------------------------------------------------")
-print("IO_LATENCY MS_RANGE DATA")
-print("-------------------------------------------------")
-pp.pprint( cachestat_data_ms)
+# print(io_latency_data)
 
-print("-------------------------------------------------")
-print("IO_LATENCY IO DATA")
-print("-------------------------------------------------")
-pp.pprint(cachestat_data_IO)
+def print_data_mysql():
+    query = "SELECT * FROM cachestat"
+    mycursor.execute(query)
+    myresult = mycursor.fetchall()
+    print(myresult)
 
-print("-------------------------------------------------")
-print("CACHESTAT DATA")
-print("-------------------------------------------------")
-pp.pprint(io_latency_data)
+
+for _ in io_latency_data:
+    # clearing the list
+    val_list = []
+
+    for key, val in _.items():
+        val_list.append(val)
+
+    # directly inserting values into the mysql
+    insert_data_cachestat_mysql(tuple(val_list)) 
+
+#--------------------------------------------------------------
+# MYSQL cachestat table (form argus_cdac database) -> structure
+# CREATE TABLE cachestat( metric_id INT NOT NULL AUTO_INCREMENT, buffers_mb INT NOT NULL, cached_mb INT NOT NULL, hits INT NOT NULL, mbd INT NOT NULL, misses INT NOT NULL, ratio INT NOT NULL, PRIMARY KEY ( metric_id ) );
+#--------------------------------------------------------------
+
 
 # JSON STRUCTURE - DATABASE OUTLINE -> FOR BETTER UNDERSTANDING ON HOW THIS CODE WORKS
 
